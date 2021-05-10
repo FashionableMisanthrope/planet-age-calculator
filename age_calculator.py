2bfcadd711a1f3dbbea13a4a7adc41e0bd2ac03c
Mark Young <fashionablemisanthrope@gmail.com>
2021-05-10 16:53:14 -0700

    created gitignore; fixed validate_birthday in age_calculator.py

--- age_calculator.py
+++ age_calculator.py
@@ -1,6 +1,7 @@
 import datetime
+import re
 
-planet_dict = [
+planet_dict = {
 	'mercury': 88,
 	'venus': 225,
 	'mars': 687,
@@ -8,22 +9,24 @@ planet_dict = [
 	'saturn': 10759,
 	'uranus': 30687,
 	'neptune': 60190
-]
+}
 
 def birthdate_prompt():
-	return str(input('What is your birthdate?'))
+	return str(input('What is your birthdate? Use (mm/dd/yyyy)\n'))
 
 def name_prompt():
-	return str(input('What is your name?'))
+	return str(input('What is your name?\n'))
 
 def validate_birthday(x):
+	birthdate_regex = re.compile(r'\d\d\/\d\d\/\d\d\d\d')
+	return birthdate_regex.search(x)		
 
 def run():
+	name = name_prompt()
 	while True:
-		name = name_prompt()
 		b_day = birthdate_prompt()
-		if validate_birthday:
+		if validate_birthday(b_day) is None:
+			print(f'That is not a valid entry, {name}. Please try again.\n')
+		else:
 			print(f'Thank you, {name}!')
 			break
-		else:
-			print(f'That is not a valid entry, {name}. Please try again.\n')
